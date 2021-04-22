39878d8b05ec8b2c94073ffd6f6fef9dd2da9be7
Isac Pederson <isacjped@gmail.com>
2021-04-22 15:53:56 -0700

    Case sensitive words (proper nouns or abbreviations i.e USA or Texas)

--- English Dictionary/dict.py
+++ English Dictionary/dict.py
@@ -4,8 +4,13 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def getDef(word):
+    word = word.lower()
     if word in data:
-        return data[word.lower()]
+        return data[word]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif word.title() in data:
+        return data[word.title()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
         if yn == "Y":
@@ -17,7 +22,7 @@ def getDef(word):
     else:
         return "The word doesn't exist. Please double check it."
 
-word = input("Enter word: ").lower()
+word = input("Enter word: ")
 
 output = (getDef(word))
 
